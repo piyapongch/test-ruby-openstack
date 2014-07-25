5ca26475b390f33b03ddf98f9c4723cd5cb8a259
Piyapong Charoenwattana <piyapong.charoenwattana@gmail.com>
2014-07-25 11:00:48 -0600

    Added file upload, download testing.

--- test/fog-openstack.rb
+++ test/fog-openstack.rb
@@ -1,7 +1,23 @@
 require 'fog'
+
+# create openstack swift service
 service = Fog::Storage.new({
   :provider            => 'OpenStack',   # OpenStack Fog provider
   :openstack_username  => 'eraadmin',      # Your OpenStack Username
   :openstack_api_key   => 'r+p7vNH&T$',      # Your OpenStack Password
+  :openstack_tenant    => 'ERA',
   :openstack_auth_url  => 'http://129.128.119.164:5000/v2.0/tokens'
 })
+
+# get container
+service.directories.get "test"
+
+# upload file
+file = directory.files.create :key => 'test.jpg', :body => File.open "test.jpg", :etag
+
+# download file
+File.open('downloaded-file.jpg', 'w') do | f |
+  directory.files.get("test.jpg") do | data, remaining, content_length |
+    f.syswrite data
+  end
+end
