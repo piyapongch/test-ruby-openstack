7f5662bcafcca075ea7579e50b2ee06a010ffb45
Piyapong Charoenwattana <piyapong.charoenwattana@gmail.com>
2014-07-25 22:40:14 -0600

    Added print metadata.

--- test/fog-openstack.rb
+++ test/fog-openstack.rb
@@ -1,23 +1,28 @@
-require 'fog'
+require "fog"
 
 # create openstack swift service
 service = Fog::Storage.new({
-  :provider            => 'OpenStack',   # OpenStack Fog provider
-  :openstack_username  => 'eraadmin',      # Your OpenStack Username
-  :openstack_api_key   => 'r+p7vNH&T$',      # Your OpenStack Password
-  :openstack_tenant    => 'ERA',
-  :openstack_auth_url  => 'http://129.128.119.164:5000/v2.0/tokens'
+  :provider            => "OpenStack",   # OpenStack Fog provider
+  :openstack_username  => "eraadmin",      # Your OpenStack Username
+  :openstack_api_key   => "r+p7vNH&T$",      # Your OpenStack Password
+  :openstack_tenant    => "ERA",
+  :openstack_auth_url  => "http://129.128.119.164:5000/v2.0/tokens"
 })
 
 # get container
 directory = service.directories.get "test"
 
 # upload file
-file = directory.files.create :key => 'test.jpg', :body => File.open("test.jpg"), :etag => nil
+file = directory.files.create :key => "test.jpg", :body => File.open("test.jpg"), :etag => nil
 
 # download file
-File.open('downloaded-file.jpg', 'w') do | f |
+File.open("downloaded-file.jpg", "w") do | f |
   directory.files.get("test.jpg") do | data, remaining, content_length |
     f.syswrite data
   end
 end
+
+# print file metadata
+file = directory.files.get("test.jpg")
+pringf("metadata: %s\n", file.metadata)
+
